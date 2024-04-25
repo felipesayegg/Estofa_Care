006b72cd50d92c7d12981ada775c96a91a31b6b2
Felipe Sayeg <felipesayeg2018@gmail.com>
2024-04-24 22:08:46 -0300

    buttonwhatapp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.example.com',
+  port:                 587,
+  user_name:            'your_username',
+  password:             'your_password',
+  authentication:       'plain',
+  enable_starttls_auto: true
+}
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
